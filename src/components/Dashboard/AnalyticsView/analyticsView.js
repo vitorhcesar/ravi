61ac4c6ac74265a5eae57d96d91cce43d38ab7e7
Vitor <thevitor.hugo27@gmail.com>
2023-05-10 21:12:17 -0300

    Commit 12

--- src/components/Dashboard/AnalyticsView/analyticsView.js
+++ src/components/Dashboard/AnalyticsView/analyticsView.js
@@ -3,7 +3,6 @@ import React from 'react'
 const AnalyticsView = () => {
     return (
         <section id='db-analyticsView' className='db-viewAnalyticsOff'>
-            <h1>ÁREA DE ANALYTICS</h1>
         </section>
     )
 }
